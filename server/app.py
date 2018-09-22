2f04c9fc21088585469bac5171d75e44c1deca06
Matt Dennis <dennis57@msu.edu>
2018-09-22 16:13:17 -0400

    add basic get for simulated user data

--- server/app.py
+++ server/app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, make_response
 app = Flask(__name__)
 
 @app.route('/')
 def index_page():
     return 'It works!'
+
+users = [
+    {
+        'username': u'matt'
+    }
+]
+
+@app.route('/api/user/<username>', methods=['GET'])
+def get_user(username):
+    user = [user for user in users if user['username'] == username]
+    if len(user) == 0:
+        abort(404)
+    return jsonify({'user': user[0]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
