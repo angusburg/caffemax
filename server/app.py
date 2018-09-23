36240d3616ea356e05fc8f1d44243aa89c5e05fb
Matt Dennis <dennis57@msu.edu>
2018-09-22 23:09:02 -0400

    rearranged endpoints to reflect new data organization

--- server/app.py
+++ server/app.py
@@ -14,33 +14,48 @@ users = [
     }
 ]
 
-coffees = [
+intakes = [
     {
-        'userId' : 1,
-        'caffeine': 200,
-        'coffeeId': 'abc123'
+        'id': 1,
+        'userId': 1,
+        'timestamp': 'time'
     },
     {
-        'userId' : 1,
+        'id': 2,
+        'userId': 1,
+        'timestamp': 'time'
+    }
+]
+
+drinks = [
+    {
+        'id' : 1,
         'caffeine': 200,
-        'coffeeId': '123abc'
+        'name': 'coffee'
     },
     {
-        'userId' : 2,
+        'id' : 2,
+        'caffeine': 100,
+        'name': 'coffee2'
+    },
+    {
+        'id' : 3,
         'caffeine': 200,
-        'coffeeId': 'abcd1234'
+        'drinkId': 'red bull'
     }
 
 ]
 
 BASE_PATH = '/api'
 
+USERS_PATH = '/users'
+
 #User operations
-@app.route(BASE_PATH + '/users', methods=['GET'])
+@app.route(BASE_PATH + USERS_PATH, methods=['GET'])
 def get_users():
     return jsonify({'users': users})
 
-@app.route(BASE_PATH + '/user', methods=['GET'])
+@app.route(BASE_PATH + USERS_PATH, methods=['GET'])
 def get_user():
     username = request.args.get('username', type = str).lower()
     user = [user for user in users if user['username'] == username]
@@ -48,7 +63,7 @@ def get_user():
         raise InvalidUsage('User not found', status_code=400)
     return jsonify({'user': user[0]})
 
-@app.route(BASE_PATH + '/user', methods=['POST'])
+@app.route(BASE_PATH + USERS_PATH, methods=['POST'])
 def create_user():
     if not request.json:
         raise InvalidUsage('Invalid Request Sent', status_code=400)
@@ -63,7 +78,7 @@ def create_user():
     users.append(user)
     return jsonify({'user': user}), 201
 
-@app.route(BASE_PATH + '/user', methods=['PUT'])
+@app.route(BASE_PATH + USERS_PATH, methods=['PUT'])
 def update_user():
     if not request.json:
         raise InvalidUsage('Invalid Request Sent', status_code=400)
@@ -73,15 +88,42 @@ def update_user():
     users.append(user)
     return jsonify({'user': user}), 201
 
-#Coffee operations
-@app.route(BASE_PATH + '/drinks', methods=['GET'])
-def get_coffee():
-    userId = request.args.get('userId', type = int)
-    drinks = [coffee for coffee in coffees if coffee['userId'] == userId]
-    if len(drinks) == 0:
-        raise InvalidUsage('No drinks found with the id: ' + str(userId), status_code=400)
-    return jsonify({'drinks': drinks})
+INTAKES_PATH = '/intakes'
 
+#intake operations
+@app.route(BASE_PATH + INTAKES_PATH, methods=['GET'])
+def get_intakes():
+    userId = request.args.get('userId', type = int)
+    intake_list = [intake for intake in intakes if intake['userId'] == userId]
+    if len(intake_list) == 0:
+        raise InvalidUsage('No intakes found with the id: ' + str(userId), status_code=400)
+    return jsonify({'intakes': intake_list})
+
+@app.route(BASE_PATH + INTAKES_PATH, methods=['POST'])
+def post_intake():
+    if not request.json:
+        raise InvalidUsage('Invalid Request Sent', status_code=400)
+
+    userId = request.json['userId']
+    id = request.json['id']
+    timestamp = request.json['timestamp']
+    intake_list = [intake for intake in intakes if intake['id'] == id]
+    if len(intake_list) > 0:
+        raise InvalidUsage('Drink already exists', status_code=400)
+    intake = {
+        'id': id,
+        'userId': userId,
+        'timestamp': timestamp
+    }
+    intakes.append(intake)
+    return jsonify({'intake': intake}), 201
+
+DRINKS_PATH = '/drinks'
+
+#Drink operations
+@app.route(BASE_PATH + DRINKS_PATH, methods=['GET'])
+def get_drinks():
+    return jsonify({'drinks': drinks})
 
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
